1b7f8f28fb3effc24a595fd77d0d36718e3e2b7c
Yosuke Yasuda <double.y.919.quick@gmail.com>
2015-10-10 23:32:31 -0700

    add bower components

--- js/main.js
+++ js/main.js
@@ -6,4 +6,37 @@ $(function () {
   $('.sub_title').textillate({
     initialDelay: 900
   });
+  var dates = [
+    new Date(2015, 8, 26),
+    new Date(2015, 8, 27),
+    new Date(2015, 8, 28),
+    new Date(2015, 8, 29),
+    new Date(2015, 8, 30),
+    new Date(2015, 9, 1),
+    new Date(2015, 9, 2),
+    new Date(2015, 9, 3),
+    new Date(2015, 9, 4),
+    new Date(2015, 9, 5),
+    new Date(2015, 9, 6),
+    new Date(2015, 9, 7),
+    new Date(2015, 9, 8),
+    new Date(2015, 9, 9),
+    new Date(2015, 9, 10)
+  ];
+  $('#article_date_picker').datetimepicker({
+    inline: true,
+    sideBySide: true,
+    showTodayButton: true,
+    toolbarPlacement: 'top',
+    useCurrent: false,
+    enabledDates: dates
+  });
+  $('#article_date_picker').on("click", "td", function(e){
+    console.log("clicked");
+  });
+  $('td.day').on("click", function(e){
+    var dataday = this.getAttribute("data-day").split("/");
+
+    location.href = dataday[1]+"-"+dataday[0]+"-"+dataday[2]+".html";
+  })
 })
